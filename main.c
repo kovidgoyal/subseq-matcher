4710ad880c3028cd898f1e663f0c5ce147cc8592
Kovid Goyal <kovid@kovidgoyal.net>
2017-05-23 09:46:37 +0530

    Fix position and src pointers broken because of re-alloc

--- main.c
+++ main.c
@@ -123,15 +123,13 @@ read_stdin(char *needle, args_info *opts) {
     size_t n = 0, needle_len = strlen(needle), max_haystack_len = 0, idx = 0;
     ssize_t sz = 0;
     int ret = 0;
-    Positions positions = {0};
     Candidates candidates = {0};
     Chars chars = {0};
 
     if (needle_len < 1) { fprintf(stderr, "Empty query not allowed.\n"); return 1; }
     ALLOC_VEC(char, chars, 8192 * 20);
-    ALLOC_VEC(int32_t, positions, 8192 * needle_len);
     ALLOC_VEC(Candidate, candidates, 8192);
-    if (chars.data == NULL || positions.data == NULL || candidates.data == NULL) return 1;
+    if (chars.data == NULL || candidates.data == NULL) return 1;
 
     while (true) {
         errno = 0;
@@ -149,26 +147,33 @@ read_stdin(char *needle, args_info *opts) {
             if (sz > 0) {
                 max_haystack_len = MAX(max_haystack_len, sz);
                 ENSURE_SPACE(char, chars, sz);
-                ENSURE_SPACE(int32_t, positions, needle_len);
                 ENSURE_SPACE(Candidate, candidates, 1);
                 memcpy(&NEXT(chars), linebuf, sz);
-                NEXT(candidates).src = &NEXT(chars);
                 NEXT(candidates).src_sz = sz - 1;
-                NEXT(candidates).positions = &NEXT(positions);
                 NEXT(candidates).idx = idx++;
-                INC(candidates, 1); INC(chars, sz); INC(positions, needle_len);
+                INC(candidates, 1); INC(chars, sz); 
             }
         }
     }
 
     Candidate *haystack = &ITEM(candidates, 0);
-
-    ret = run_scoring(haystack, 0, SIZE(candidates), needle, needle_len, max_haystack_len, opts->level1_arg, opts->level2_arg, opts->level3_arg);
-    output_results(haystack, SIZE(candidates), opts, needle_len);
+    int32_t *positions = (int32_t*)calloc(SIZE(candidates), sizeof(int32_t) * needle_len);
+    if (positions) {
+        char *cdata = &ITEM(chars, 0);
+        for (size_t i = 0, off = 0; i < SIZE(candidates); i++) {
+            haystack[i].positions = positions + (i * needle_len);
+            haystack[i].src = cdata + off;
+            off += haystack[i].src_sz + 1;
+        }
+        ret = run_scoring(haystack, 0, SIZE(candidates), needle, needle_len, max_haystack_len, opts->level1_arg, opts->level2_arg, opts->level3_arg);
+        output_results(haystack, SIZE(candidates), opts, needle_len);
+    } else {
+        ret = 1; REPORT_OOM;
+    }
 
     if (linebuf) free(linebuf);
     linebuf = NULL;
-    FREE_VEC(chars); FREE_VEC(positions); FREE_VEC(candidates);
+    FREE_VEC(chars); free(positions); FREE_VEC(candidates);
     return ret;
 }
 
